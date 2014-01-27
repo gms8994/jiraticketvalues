66103b14331f90fccc9ff4072f2528c455eb5032
Brent Selch <bselch@learninghouse.com>
2014-01-27 11:40:36 -0500

    Adding Safari required metadata Add delay and isNaN checks

--- ticketvalues.js
+++ ticketvalues.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
+$(document).ready(setTimeout(function() {
 	var headers = $('.ghx-column').not('.ui-sortable');
 	var swimlanes = $('.ghx-column.ui-sortable');
-
 	swimlanes.each(function(idx, val) {
 		var sum = 0;
 		$(val).find('div.ghx-issue .ghx-corner .aui-badge').each(function(idx, val) {
-			sum += parseInt($(val).text());
+			if (!isNaN(parseInt($(val).text()))) {
+				sum += parseInt($(val).text());
+			}
 		});
 
 		$(headers[idx]).prepend('<div class="ghx-end" style="float:right;"><div class="ghx-corner"><span class="aui-badge" title="Total Story Points">' + sum + '</span></div></div>');
 	});
-});
+},2000));
